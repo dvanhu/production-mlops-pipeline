1412321f0bd0d770aea2439366d35513ed500c34
Divanshu <114694978+dvanhu@users.noreply.github.com>
2026-04-24 11:11:27 +0530

    Update model_evaluator.py

--- steps/training/model_evaluator.py
+++ steps/training/model_evaluator.py
@@ -1,20 +1,4 @@
-# Apache Software License 2.0
-# 
-# Copyright (c) ZenML GmbH 2026. All rights reserved.
-# 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-# http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# 
-
+import os
 import mlflow
 import pandas as pd
 from sklearn.base import ClassifierMixin
@@ -52,8 +36,11 @@ def model_evaluator(
     logger.info(f"Test accuracy={tst_acc*100:.2f}%")
 
     # -----------------------------
-    # FIX: Explicit MLflow Handling
+    # FIX: Stable MLflow Setup
     # -----------------------------
+    os.makedirs("mlruns", exist_ok=True)
+
+    mlflow.set_tracking_uri("file:./mlruns")
     mlflow.set_experiment("e2e_use_case_training")
 
     with mlflow.start_run():
